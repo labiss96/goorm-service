245b49d4ea85be18f5a8b03d6873a3308ab3940d
labiss96 <cxz9080@gmail.com>
2020-06-28 14:22:37 +0900

    implement add brand function in Dialog.

---
+++ goorm-frontend/goorm-app/src/components/product/BrandFromDialog.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import product_api from "api/ProductAPI";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+// import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function BrandFormDialog(props) {
+  const [open, setOpen] = useState(false);
+  const [brand, setBrand] = useState("");
+
+  //   useEffect(() => {
+
+  //   });
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const onSubmit = async () => {
+    await product_api
+      .createBrand({ name: brand })
+      .then((result) => {
+        console.log(result);
+        handleClose();
+        props.get_brands();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Add brand
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Add Brand</DialogTitle>
+        <DialogContent>
+          {/* <DialogContentText>추가할 브랜드 명을 입력하세요</DialogContentText> */}
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Brand Name"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={onSubmit} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- goorm-frontend/goorm-app/src/components/product/ProductForm.jsx
+++ goorm-frontend/goorm-app/src/components/product/ProductForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import {
   Button,
   Grid,
@@ -9,9 +9,15 @@ import {
   Select,
   MenuItem,
   Typography,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
 } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import product_api from "api/ProductAPI";
+import BrandFormDialog from "./BrandFromDialog";
 
 const useStyles = (theme) => ({
   paper: {
@@ -41,10 +47,11 @@ class ProductForm extends Component {
     nicotine: 0.0,
     tar: 0.0,
     throat_hit: "",
-    isMenthol: false,
+    isMenthol: "",
     brand: -1,
     brand_list: [],
     selectOpen: false,
+    is_successful: false,
   };
 
   componentDidMount = () => {
@@ -68,129 +75,186 @@ class ProductForm extends Component {
     console.log(event.target.value);
   };
 
-  createTobacco = async () => {
-    // var product_data = {
-    // };
-    await product_api
-      .createTobacco({
-        name: this.state.name,
-        price: this.state.price,
-        nicotine: this.state.nicotine,
-        tar: this.state.tar,
-        throat_hit: this.state.throat_hit,
-        isMenthol: this.state.isMenthol,
-        brand: this.state.brand,
-      })
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => console.log(err));
+  createTobacco = async (event) => {
+    event.preventDefault();
+    if (this.state.throat_hit === "" || this.state.brand === -1) {
+      alert("모든 항목을 채워주세요");
+    } else {
+      await product_api
+        .createTobacco({
+          name: this.state.name,
+          price: this.state.price,
+          nicotine: this.state.nicotine,
+          tar: this.state.tar,
+          throat_hit: this.state.throat_hit,
+          isMenthol: this.state.isMenthol,
+          brand: this.state.brand,
+        })
+        .then((result) => {
+          console.log(result);
+          this.setState({
+            is_successful: true,
+          });
+          alert("성공적으로 제품이 추가되었습니다.");
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   render() {
     const { classes } = this.props;
     return (
-      <Container maxWidth="xs">
-        <Paper className={classes.paper} elevation={0}>
-          <Typography component="h1" variant="h5">
-            Add Product
-          </Typography>
-          <form onSubmit={this.createTobacco} className={classes.form}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Select
-                  labelId="usertype-select-label"
-                  id="usertype-controlled-open-select"
-                  open={this.state.selectOpen}
-                  onClose={(e) => this.setState({ selectOpen: false })}
-                  name="brand"
-                  onOpen={(e) => this.setState({ selectOpen: true })}
-                  value={this.state.brand}
-                  onChange={(e) => this.setState({ brand: e.target.value })}
-                >
-                  <MenuItem value={-1}>Select Brand</MenuItem>
-                  {this.state.brand_list.map((brd) => (
-                    <MenuItem key={brd.id} value={brd.id}>
-                      {brd.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  name="name"
-                  label="product name"
-                  value={this.state.name}
-                  onChange={this.handlingChange}
-                  required
-                  fullWidth
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="price"
-                  label="price"
-                  type="number"
-                  value={this.state.price}
-                  onChange={this.handlingChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="nicotine"
-                  label="nicotine"
-                  type="number"
-                  step="0.1"
-                  value={this.state.nicotine}
-                  onChange={this.handlingChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="tar"
-                  label="TAR"
-                  type="number"
-                  step="0.1"
-                  value={this.state.tar}
-                  onChange={this.handlingChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="throat_hit"
-                  label="throat hit"
-                  value={this.state.throat_hit}
-                  onChange={this.handlingChange}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
+      <>
+        {this.state.is_successful ? <Redirect to="/product" /> : <></>}
+
+        <Container maxWidth="xs">
+          <Paper className={classes.paper} elevation={0}>
+            <Typography component="h1" variant="h5">
+              Add Product
+            </Typography>
+            <form
+              onSubmit={(e) => this.createTobacco(e)}
+              className={classes.form}
             >
-              ADD
-            </Button>
-          </form>
-          <Link to="/product">Cancel</Link>
-        </Paper>
-      </Container>
+              <Grid container spacing={2}>
+                <Grid item xs={8}>
+                  <Select
+                    labelId="usertype-select-label"
+                    id="usertype-controlled-open-select"
+                    open={this.state.selectOpen}
+                    onClose={(e) => this.setState({ selectOpen: false })}
+                    name="brand"
+                    onOpen={(e) => this.setState({ selectOpen: true })}
+                    value={this.state.brand}
+                    onChange={(e) => this.setState({ brand: e.target.value })}
+                  >
+                    <MenuItem value={-1}>Select Brand</MenuItem>
+                    {this.state.brand_list.map((brd) => (
+                      <MenuItem key={brd.id} value={brd.id}>
+                        {brd.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </Grid>
+                <Grid item xs={4}>
+                  <BrandFormDialog get_brands={this.getBrands} />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    name="name"
+                    label="product name"
+                    value={this.state.name}
+                    onChange={this.handlingChange}
+                    required
+                    fullWidth
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="price"
+                    label="price"
+                    type="number"
+                    value={this.state.price}
+                    onChange={this.handlingChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="nicotine"
+                    label="nicotine"
+                    type="number"
+                    step="0.1"
+                    value={this.state.nicotine}
+                    onChange={this.handlingChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="tar"
+                    label="TAR"
+                    type="number"
+                    step="0.1"
+                    value={this.state.tar}
+                    onChange={this.handlingChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl component="fieldset">
+                    <FormLabel component="legend">Throat hit</FormLabel>
+                    <RadioGroup
+                      aria-label="throat_hit"
+                      name="throat_hit"
+                      required
+                      value={this.state.throat_hit}
+                      onChange={this.handlingChange}
+                    >
+                      <FormControlLabel
+                        value="Low"
+                        control={<Radio />}
+                        label="Low"
+                      />
+                      <FormControlLabel
+                        value="Medium"
+                        control={<Radio />}
+                        label="Medium"
+                      />
+                      <FormControlLabel
+                        value="High"
+                        control={<Radio />}
+                        label="High"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl component="fieldset">
+                    <FormLabel component="legend">Menthol Type</FormLabel>
+                    <RadioGroup
+                      aria-label="Menthol Type"
+                      name="isMenthol"
+                      required
+                      value={this.state.isMenthol}
+                      onChange={this.handlingChange}
+                    >
+                      <FormControlLabel
+                        value="YES"
+                        control={<Radio />}
+                        label="Menthol"
+                      />
+                      <FormControlLabel
+                        value="NO"
+                        control={<Radio />}
+                        label="Non-Menthol"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                ADD
+              </Button>
+            </form>
+            <Link to="/product">Cancel</Link>
+          </Paper>
+        </Container>
+      </>
     );
   }
 }
